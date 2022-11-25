e7ebf2ac49c475f90136a61d1b258c2385f6bf72
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-25 21:00:56 +0530

    add page added

---
+++ src/app/friends-add/friends-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friends-add',
+  templateUrl: './friends-add.component.html',
+  styleUrls: ['./friends-add.component.css']
+})
+export class FriendsAddComponent {
+
+}
